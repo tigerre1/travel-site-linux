149df66aca947120042dd22921d1e737beb72d5c
René Tigerstedt <rene.tigerstedt@gmail.com>
2017-08-25 14:13:29 +0300

    mixins and media queries

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,13 +3,14 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, autoprefixer, cssvars, nested ]))
+    .pipe(postcss([cssImport, mixins, autoprefixer, cssvars, nested ]))
     .on('error', function(errorInfo){
       console.log(errorInfo.toString())
       this.emit('end');
